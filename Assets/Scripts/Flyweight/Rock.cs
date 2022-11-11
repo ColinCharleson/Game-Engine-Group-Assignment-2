78cfd4f147c7ee17facbe055a8ae4edc7a59b602
Suraaj <surdige@gmail.com>
2022-11-11 16:03:06 -0500

    FlyweightRocks

---
+++ Assets/Scripts/Flyweight/Rock.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace FlyweightPattern
+{
+
+    public class Rock : MonoBehaviour
+    {
+        public List<Vector2> rockPosition;
+        
+
+    }
+}
